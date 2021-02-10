f5d3a52498b2dc2b3131a0ffcd03f0aa23f5014d
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2021-02-09 18:03:51 -0800

    add pooping and clean up poop

--- src/gameState.js
+++ src/gameState.js
@@ -1,4 +1,4 @@
-import { modFox, modScene } from "./ui";
+import { modFox, modScene, togglePoopBag } from "./ui";
 import {
   RAIN_CHANCE,
   SCENES,
@@ -16,11 +16,12 @@ const gameState = {
   sleepTime: -1,
   hungryTime: -1,
   dieTime: -1,
+  poopTime: -1,
   timeToStartCelebrating: -1,
   timeToEndCelebrating: -1,
   tick() {
     this.clock++;
-    console.log("click", this.clock);
+    console.log("click", this.clock, this);
 
     if (this.clock === this.wakeTime) {
       this.wake();
@@ -34,6 +35,8 @@ const gameState = {
       this.startCelebrating();
     } else if (this.clock === this.timeToEndCelebrating) {
       this.endCelebrating();
+    } else if (this.clock === this.poopTime) {
+      this.poop();
     }
     return this.clock;
   },
@@ -64,6 +67,12 @@ const gameState = {
     this.hungryTime = -1;
     modFox("hungry");
   },
+  poop() {
+    this.current = "POOPING";
+    this.poopTime = -1;
+    this.dieTime = getNextDieTime(this.clock);
+    modFox("pooping");
+  },
   die() {
     console.log("die");
   },
@@ -77,6 +86,7 @@ const gameState = {
     this.timeToEndCelebrating = -1;
     this.current = "IDLING";
     this.determineFoxState();
+    togglePoopBag(false);
   },
   determineFoxState() {
     if (this.current === "IDLING") {
@@ -118,7 +128,14 @@ const gameState = {
     console.log("change weather");
   },
   cleanUpPoop() {
-    console.log("clean up poop");
+    if (this.current !== "POOPING") {
+      return;
+    }
+
+    this.dieTime = -1;
+    togglePoopBag(true);
+    this.startCelebrating();
+    getNextHungerTime(this.clock);
   },
   feed() {
     if (this.current !== "HUNGRY") {
